595e1a9d5e3ee7ad407790eb0c427655b27a5d39
confi-n <confidentialnri@gmail.com>
2015-07-16 20:35:19 -0400

    Hello World C Program ! - Updated on 16th Jule 2015, 20:35

--- helloworld.c
+++ helloworld.c
@@ -3,4 +3,9 @@
 void main()
 {
     printf("Hello, Planet Earth ! :P\n");
+
+    double a, b;
+    printf("Enter two nos. to add:\n");
+    scanf("%lf %lf", &a, &b);
+    printf("Result of addition is %lf\n", a+b);
 }
